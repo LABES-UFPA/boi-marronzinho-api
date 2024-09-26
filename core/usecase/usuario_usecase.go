8013aba895279a6d4906af360f026357e9cb29c8
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-26 18:51:01 -0300

    feat: criando core do usuario

--- core/usecase/usuario_usecase.go
+++ core/usecase/usuario_usecase.go
@@ -1,1 +1,54 @@
 package usecase
+
+import (
+	"boi-marronzinho-api/adapter/repository"
+	"boi-marronzinho-api/domain"
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type UsuarioUseCase struct {
+	userRepo repository.UserRepository
+}
+
+func NewUsuarioUseCase(userRepo repository.UserRepository) *UsuarioUseCase {
+	return &UsuarioUseCase{userRepo: userRepo}
+}
+
+func (uc *UsuarioUseCase) CreateUser(usuarioRequest *domain.Usuario) (*domain.Usuario, error) {
+	existingUser, err := uc.userRepo.GetByEmail(usuarioRequest.Email)
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+	if existingUser != nil {
+		return nil, errors.New("email já está em uso")
+	}
+
+	// hashedPassword, err := uc.HashPassword(usuarioRequest.Password)
+	// if err != nil {
+	// 	return nil, errors.New("erro ao gerar hash da senha")
+	// }
+
+	user := &domain.Usuario{
+		ID:           uuid.New(),
+		Nome: usuarioRequest.Nome,
+		Email: usuarioRequest.Email,
+		TipoUsuario: usuarioRequest.TipoUsuario,
+		IdiomaPreferido: usuarioRequest.TipoUsuario,
+		CriadoEm: time.Now(),
+	}
+
+	// err = user.Validate()
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	if err = uc.userRepo.Create(user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
