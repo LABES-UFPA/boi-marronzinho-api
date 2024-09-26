058ce14f43563ed12d53d10cf9653bcafa9ef8e6
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-26 18:51:17 -0300

    feat: model do usuario

--- domain/usuario.go
+++ domain/usuario.go
@@ -1,1 +1,15 @@
 package domain
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type Usuario struct {
+	ID             uuid.UUID      `json:"id"`                        
+	Nome           string         `json:"nome"`                    
+	Email          string         `json:"email"`                   
+	TipoUsuario    string         `json:"tipo_usuario"`     
+	IdiomaPreferido string        `json:"idioma_preferido"`
+	CriadoEm       time.Time      `json:"criado_em"`
+}
