0b00ee873cdcdeb422733d5f3d9db1f788e0af7d
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-26 18:50:13 -0300

    feat: adicionando rota de criação de usuário

--- adapter/http/router/usuario_router.go
+++ adapter/http/router/usuario_router.go
@@ -1,13 +1,17 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"boi-marronzinho-api/adapter/http/handler"
+
+	"github.com/gin-gonic/gin"
+)
 
 
 func SetupUsuarioRoutes(router *gin.Engine, userHandler *handler.UsuarioHandler) {
 	userGroup := router.Group("/api/v1/usuarios")
 	{
 		userGroup.POST("", userHandler.CreateUser)
-		userGroup.POST("/login", userHandler.Login)
-		userGroup.GET("/:id", userHandler.GetUser)
+		// userGroup.POST("/login", userHandler.Login)
+		// userGroup.GET("/:id", userHandler.GetUser)
 	}
 }
