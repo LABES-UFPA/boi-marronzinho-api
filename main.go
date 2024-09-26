33b4aafb958d76f44fc0149943bf8967e6348a90
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-26 18:48:54 -0300

    feat: criando a main com fx e gin para inicialização do serviço

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"boi-marronzinho-api/adapter/http"
+	"boi-marronzinho-api/module/usuario"
+	"boi-marronzinho-api/postgres"
 	"go.uber.org/fx"
 )
 
 func main() {
 	app := fx.New(
-        fx.Provide(
-        ),
-        fx.Invoke(), 
-    )
+		fx.Provide(postgres.InitDB),
+		http.RouterModule(),
+		usuario.UserModule,
+		fx.Invoke(http.RegisterRoutes),
+	)
 
-    app.Run()
+	app.Run()
 }
