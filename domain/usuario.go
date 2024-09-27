cff5108bad1a47f243ad25366fb14f20fea9d658
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-27 16:40:41 -0300

    fix: ajustando model de usuário, adicionando tag do gorm + função de table name

--- domain/usuario.go
+++ domain/usuario.go
@@ -1,15 +1,19 @@
 package domain
 
 import (
-	"time"
 	"github.com/google/uuid"
+	"time"
 )
 
 type Usuario struct {
-	ID             uuid.UUID      `json:"id"`                        
-	Nome           string         `json:"nome"`                    
-	Email          string         `json:"email"`                   
-	TipoUsuario    string         `json:"tipo_usuario"`     
-	IdiomaPreferido string        `json:"idioma_preferido"`
-	CriadoEm       time.Time      `json:"criado_em"`
+	ID              uuid.UUID `json:"id" gorm:"primaryKey"`
+	Nome            string    `json:"nome"`
+	Email           string    `json:"email"`
+	TipoUsuario     string    `json:"tipo_usuario"`
+	IdiomaPreferido string    `json:"idioma_preferido"`
+	CriadoEm        time.Time `json:"criado_em"`
+}
+
+func (d *Usuario) TableName() string {
+	return "boi_marronzinho.usuarios"
 }
