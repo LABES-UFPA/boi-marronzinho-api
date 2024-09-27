bedaded590c1a88d3a50cab5d672498934b9ec74
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-27 16:40:01 -0300

    feat: add função para inicializar o servidor fx+gin

--- adapter/http/router.go
+++ adapter/http/router.go
@@ -3,10 +3,30 @@ package http
 import (
 	"boi-marronzinho-api/adapter/http/handler"
 	"boi-marronzinho-api/adapter/http/router"
-
+	"context"
 	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
 	"go.uber.org/fx"
 )
+
+func StartServer(lc fx.Lifecycle, r *gin.Engine) {
+	port := ":8080"
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			go func() {
+				logrus.Infof("API rodando na porta %s", port)
+				if err := r.Run(port); err != nil {
+					logrus.Errorf("Erro ao iniciar o servidor: %v", err)
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logrus.Info("API foi finalizada")
+			return nil
+		},
+	})
+}
 
 func RegisterRoutes(
 	r *gin.Engine,
@@ -20,5 +40,8 @@ func SetupRouter() *gin.Engine {
 }
 
 func RouterModule() fx.Option {
-	return fx.Provide(SetupRouter)
+	return fx.Options(
+		fx.Provide(SetupRouter),
+		fx.Invoke(StartServer),
+	)
 }
