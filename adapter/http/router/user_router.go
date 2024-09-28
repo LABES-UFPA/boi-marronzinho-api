f93278ad0b710322b0f477304098786882eebb12
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-28 13:23:24 -0300

    feat(user routes): add rotas publicas de login e cadastro e crinado grupo de rotas protegidas.

---
+++ adapter/http/router/user_router.go
@@ -0,0 +1,22 @@
+package router
+
+import (
+	"boi-marronzinho-api/adapter/http/handler"
+	"boi-marronzinho-api/auth"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupUserRoutes(router *gin.Engine, userHandler *handler.UserHandler) {
+	publicGroup := router.Group("/api/v1/usuarios")
+	{
+		publicGroup.POST("/signup", userHandler.CreateUser)
+		publicGroup.POST("/login", userHandler.Login)
+	}
+
+	protectedGroup := router.Group("/api/v1/usuarios")
+	protectedGroup.Use(auth.JWTAuthMiddleware())
+	{
+		//protectedGroup.GET("/:id", userHandler.GetUser)
+	}
+}
