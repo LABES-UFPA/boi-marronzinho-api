c2675452b5d52417487e2392353c3486ff864d50
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-28 13:21:19 -0300

    fix: ajuste na migration

--- migrate/changelogs/V202409280957380__add_modifications_to_user_table.sql
+++ migrate/changelogs/V202409280957380__add_modifications_to_user_table.sql
@@ -1,43 +1,35 @@
--- Adiciona a coluna para armazenar o hash da senha, permitindo nulos temporariamente
 ALTER TABLE usuarios
 ADD COLUMN password_hash VARCHAR(255);
 
--- Preencher a coluna password_hash com um valor padrão para registros existentes
--- ATENÇÃO: Substitua 'dummy_hash' por algo apropriado se desejar obrigar a redefinição de senha.
 UPDATE usuarios
 SET password_hash = 'dummy_hash'
 WHERE password_hash IS NULL;
 
--- Altera a coluna para NOT NULL após todos os valores serem preenchidos
 ALTER TABLE usuarios
 ALTER COLUMN password_hash SET NOT NULL;
 
--- Altera a coluna tipo_usuario para ter um tamanho maior e ser mais flexível
 ALTER TABLE usuarios
 ALTER COLUMN tipo_usuario SET DATA TYPE VARCHAR(20),
 ALTER COLUMN tipo_usuario SET DEFAULT 'Cliente';
 
--- Adiciona a coluna last_login para registrar o último login do usuário
 ALTER TABLE usuarios
 ADD COLUMN last_login TIMESTAMP;
 
--- Adiciona a coluna password_reset_token para armazenar o token de redefinição de senha
 ALTER TABLE usuarios
 ADD COLUMN password_reset_token VARCHAR(255);
 
--- Adiciona a coluna password_reset_expires para registrar a data de expiração do token de redefinição
 ALTER TABLE usuarios
 ADD COLUMN password_reset_expires TIMESTAMP;
 
--- Adiciona a coluna deleted_at para gerenciar a deleção lógica (soft delete)
 ALTER TABLE usuarios
 ADD COLUMN deleted_at TIMESTAMP;
 
--- Atualiza o valor padrão da coluna criado_em para usar o CURRENT_TIMESTAMP
 ALTER TABLE usuarios
-ALTER COLUMN criado_em SET DEFAULT CURRENT_TIMESTAMP;
+RENAME COLUMN criado_em TO created_at;
 
--- Adiciona a coluna updated_at se ela não existir
+ALTER TABLE usuarios
+ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP;
+
 DO $$
 BEGIN
     IF NOT EXISTS (
@@ -49,6 +41,19 @@ BEGIN
     END IF;
 END $$;
 
--- (Opcional) Atualiza o valor padrão da coluna updated_at se ela já existir
 ALTER TABLE usuarios
 ALTER COLUMN updated_at SET DEFAULT CURRENT_TIMESTAMP;
+
+ALTER TABLE usuarios
+ADD COLUMN first_name VARCHAR(50) NOT NULL,
+ADD COLUMN last_name VARCHAR(50) NOT NULL;
+
+UPDATE usuarios
+SET first_name = split_part(nome, ' ', 1),
+    last_name = CASE
+        WHEN length(split_part(nome, ' ', 2)) > 0 THEN substring(nome from position(' ' in nome) + 1)
+        ELSE 'Sobrenome não informado'
+    END;
+
+ALTER TABLE usuarios
+DROP COLUMN nome;
