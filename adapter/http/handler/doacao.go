966146e9bda72097ca252a9c2205eb6061f9f465
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-28 18:55:40 -0300

    wip(doação router): construindo handler para realizar a doação de um objeto.

---
+++ adapter/http/handler/doacao.go
@@ -0,0 +1,33 @@
+package handler
+
+import (
+	"boi-marronzinho-api/core/usecase"
+	"boi-marronzinho-api/domain"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type DoacaoHandler struct {
+	DoacaoUseCase *usecase.DoacaoUseCase
+}
+
+func NewDoacaoHandler(duc *usecase.DoacaoUseCase) *DoacaoHandler {
+	return &DoacaoHandler{DoacaoUseCase: duc}
+}
+
+func (dh *DoacaoHandler) AdicionaDoacao(c *gin.Context) {
+	var doacaoDTO *domain.Doacoes
+	if err := c.ShouldBindJSON(&doacaoDTO); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	doacao, err := dh.DoacaoUseCase.AdicionaDoacao(doacaoDTO)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, doacao)
+}
