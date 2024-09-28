12ff862cfc75f0f450ee9eff896a32e3befc7cd2
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-09-28 18:28:11 -0300

    feat(doacao): criando usecase responsavel por adcionar uma docao feita por um usuario e adicionar sua boicoins

---
+++ core/usecase/doacao_usecase.go
@@ -0,0 +1,47 @@
+package usecase
+
+import (
+	"boi-marronzinho-api/adapter/repository"
+	"boi-marronzinho-api/domain"
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type DoacaoUseCase struct {
+	doacaoRepo repository.DoacaoRepository
+}
+
+func NewDoacaoRepository(doacaoRepo repository.DoacaoRepository) *DoacaoUseCase {
+	return &DoacaoUseCase{doacaoRepo: doacaoRepo}
+}
+
+func (duc *DoacaoUseCase) AdicionaDoacao(doacaoResquest *domain.Doacoes) (*domain.Doacoes, error) {
+
+	itemDoacao, err := duc.doacaoRepo.CapturaItemDoacao(doacaoResquest.ItemDoacaoID)
+	if err != nil {
+		return nil, nil
+	}
+
+	boicoinsRecebidos, err := calculaBoicoins(itemDoacao.BoicoinsUnidade, doacaoResquest.Quantidade)
+	if err != nil {
+		return nil, nil
+	}
+
+	doacao := &domain.Doacoes{
+		ID:                uuid.New(),
+		UsuarioID:         doacaoResquest.UsuarioID,
+		ItemDoacaoID:      doacaoResquest.ItemDoacaoID,
+		BoicoinsRecebidos: boicoinsRecebidos,
+		DataDoacao:        time.Now(),
+	}
+	return doacao, nil
+}
+
+func calculaBoicoins(valorUnidade float64, quantidade int64) (float64, error) {
+	if valorUnidade < 0 || quantidade < 0 {
+		return 0, errors.New("valorUnidade e quantidade devem ser maiores que zero")
+	}
+	return valorUnidade * float64(quantidade), nil
+}
