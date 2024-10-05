2ad123f94d69023d2ed09cc9275c270335e65ac7
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-05 11:32:07 -0300

    fix(module fx): ajustando modulos fx para usar repository genérco

--- module/doacao/doacao_module.go
+++ module/doacao/doacao_module.go
@@ -1,28 +1,34 @@
 package doacao
 
 import (
-	"boi-marronzinho-api/adapter/http/handler"
-	"boi-marronzinho-api/adapter/repository"
-	"boi-marronzinho-api/core/usecase"
+    "boi-marronzinho-api/adapter/http/handler"
+    "boi-marronzinho-api/adapter/repository"
+    "boi-marronzinho-api/core/usecase"
+    "boi-marronzinho-api/domain"
 
-	"go.uber.org/fx"
-	"gorm.io/gorm"
+    "go.uber.org/fx"
+    "gorm.io/gorm"
 )
 
 var DoacaoModule = fx.Options(
-	fx.Provide(NewDoacaoRepository),
-	fx.Provide(NewDoacaoUseCase),
-	fx.Provide(NewDoacaoHandler),
+    fx.Provide(NewDoacaoRepository),
+    fx.Provide(NewItemDoacaoRepository),
+    fx.Provide(NewDoacaoUseCase),
+    fx.Provide(NewDoacaoHandler),
 )
 
-func NewDoacaoRepository(db *gorm.DB) repository.DoacaoRepository {
-	return repository.NewDoacaoRepository(db)
+func NewDoacaoRepository(db *gorm.DB) repository.Repository[domain.Doacoes] {
+    return repository.NewRepository[domain.Doacoes](db)
 }
 
-func NewDoacaoUseCase(repo repository.DoacaoRepository) *usecase.DoacaoUseCase {
-	return usecase.NewDoacaoUseCase(repo)
+func NewItemDoacaoRepository(db *gorm.DB) repository.Repository[domain.ItemDoacao] {
+    return repository.NewRepository[domain.ItemDoacao](db)
+}
+
+func NewDoacaoUseCase(doacaoRepo repository.Repository[domain.Doacoes], itemDoacaoRepo repository.Repository[domain.ItemDoacao]) *usecase.DoacaoUseCase {
+    return usecase.NewDoacaoUseCase(doacaoRepo, itemDoacaoRepo)
 }
 
 func NewDoacaoHandler(duc *usecase.DoacaoUseCase) *handler.DoacaoHandler {
-	return handler.NewDoacaoHandler(duc)
+    return handler.NewDoacaoHandler(duc)
 }
