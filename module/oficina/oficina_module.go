6935eea9edd818f94a089d26cb61b9888f529eb3
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-08 15:18:12 -0300

    feat(module fx): criando module fx para oficina

--- module/oficina/oficina_module.go
+++ module/oficina/oficina_module.go
@@ -1,1 +1,28 @@
 package oficina
+
+import (
+	"boi-marronzinho-api/adapter/http/handler"
+	"boi-marronzinho-api/adapter/repository"
+	"boi-marronzinho-api/core/usecase"
+
+	"go.uber.org/fx"
+	"gorm.io/gorm"
+)
+
+var OficnaModule = fx.Options(
+	fx.Provide(NewOficinaRepository),
+	fx.Provide(NewOficinaUseCase),
+	fx.Provide(NewOficinaHandler),
+)
+
+func NewOficinaRepository(db *gorm.DB) repository.OficinaRepository {
+	return repository.NewOficinaRepository(db)
+}
+
+func NewOficinaUseCase(oficinaRepo repository.OficinaRepository, usuarioRepo repository.UserRepository) *usecase.OficinaUseCase {
+	return usecase.NewOficinaUseCase(oficinaRepo, usuarioRepo)
+}
+
+func NewOficinaHandler(uc *usecase.OficinaUseCase) *handler.OficinaHandler {
+	return handler.NewOficinaHandler(uc)
+}
