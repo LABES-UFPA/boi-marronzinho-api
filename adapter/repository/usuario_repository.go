b25b76e6b97140f6a79ad7ccae950ec8688fed89
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-18 13:38:04 -0300

    feat(repository): criando metodo para filtrar usuarios pelo nome

--- adapter/repository/usuario_repository.go
+++ adapter/repository/usuario_repository.go
@@ -9,48 +9,59 @@ import (
 )
 
 type UserRepository interface {
-    Repository[domain.Usuario]
-    GetByEmail(email string) (*domain.Usuario, error)
-    AtualizarSaldo(usuarioID uuid.UUID, boicoinsRecebidos float64) error
-    GetExtrato(usuarioID uuid.UUID)([]*domain.BoicoinsTransacoes, error)
+	Repository[domain.Usuario]
+	GetByEmail(email string) (*domain.Usuario, error)
+	AtualizarSaldo(usuarioID uuid.UUID, boicoinsRecebidos float64) error
+	GetExtrato(usuarioID uuid.UUID) ([]*domain.BoicoinsTransacoes, error)
+	GetByFullName(name string) ([]*domain.Usuario, error)
 }
 
 type userRepository struct {
-    Repository[domain.Usuario]
-    db *gorm.DB
+	Repository[domain.Usuario]
+	db *gorm.DB
 }
 
 func NewUserRepository(db *gorm.DB) UserRepository {
-    return &userRepository{
-        Repository: NewRepository[domain.Usuario](db),
-        db: db,
-    }
+	return &userRepository{
+		Repository: NewRepository[domain.Usuario](db),
+		db:         db,
+	}
 }
 
 func (ur *userRepository) GetByEmail(email string) (*domain.Usuario, error) {
-    var user domain.Usuario
-    if err := ur.db.Where("email = ?", email).First(&user).Error; err != nil {
-        return nil, err
-    }
-    return &user, nil
+	var user domain.Usuario
+	if err := ur.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
 
 func (ur *userRepository) AtualizarSaldo(usuarioID uuid.UUID, boicoinsRecebidos float64) error {
-    result := ur.db.Model(&domain.Usuario{}).Where("id = ?", usuarioID).
-        Update("saldo_boicoins", gorm.Expr("saldo_boicoins + ?", boicoinsRecebidos))
-    if result.Error != nil {
-        return result.Error
-    }
-    if result.RowsAffected == 0 {
-        return errors.New("usuário não encontrado ou saldo não atualizado")
-    }
-    return nil
+	result := ur.db.Model(&domain.Usuario{}).Where("id = ?", usuarioID).
+		Update("saldo_boicoins", gorm.Expr("saldo_boicoins + ?", boicoinsRecebidos))
+	if result.Error != nil {
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		return errors.New("usuário não encontrado ou saldo não atualizado")
+	}
+	return nil
 }
 
-func (ur *userRepository) GetExtrato(usuarioID uuid.UUID)([]*domain.BoicoinsTransacoes, error) {
-    var transacoes []*domain.BoicoinsTransacoes
-    if err := ur.db.Where("usuario_id =?", usuarioID).Find(&transacoes).Error; err!= nil {
-        return nil, err
-    }
-    return transacoes, nil
+func (ur *userRepository) GetExtrato(usuarioID uuid.UUID) ([]*domain.BoicoinsTransacoes, error) {
+	var transacoes []*domain.BoicoinsTransacoes
+	if err := ur.db.Where("usuario_id =?", usuarioID).Find(&transacoes).Error; err != nil {
+		return nil, err
+	}
+	return transacoes, nil
+}
+
+func (ur *userRepository) GetByFullName(name string) ([]*domain.Usuario, error) {
+	var users []*domain.Usuario
+	query := "%" + name + "%"
+	if err := ur.db.Where("CONCAT(first_name, ' ', last_name) LIKE ?", query).Find(&users).Error; err != nil {
+		return nil, err
+	}
+
+	return users, nil
 }
