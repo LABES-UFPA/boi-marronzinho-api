3cf7eb97bb9212cf34faecbb1b6d8148e4a8d2a1
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-20 15:37:35 -0300

    feat(module fx): criando module fx para carteira de boicoins

--- module/boicoin/boicoin_module.go
+++ module/boicoin/boicoin_module.go
@@ -1,16 +1,30 @@
 package boicoin
 
 import (
+	"boi-marronzinho-api/adapter/http/handler"
 	"boi-marronzinho-api/adapter/repository"
+	"boi-marronzinho-api/core/usecase"
 
 	"go.uber.org/fx"
 	"gorm.io/gorm"
 )
 
-var BoincoinModule = fx.Options(
+var BoicoinModule = fx.Options(
     fx.Provide(NewBoicoinRepository),
+    fx.Provide(NewCarteiraHandler),
+    fx.Provide(NewCarteiraUseCase),
 )
 
 func NewBoicoinRepository(db *gorm.DB) repository.BoicoinRepository {
     return repository.NewBoicoinRepository(db)
+}
+
+func NewCarteiraHandler(cuc *usecase.CarteiraUseCase) *handler.CarteiraHandler {
+    return handler.NewCarteiraHandler(cuc)
+}
+
+func NewCarteiraUseCase(
+    boicoinRepo repository.BoicoinRepository,
+) *usecase.CarteiraUseCase {
+    return usecase.NewCarteiraUseCase(boicoinRepo)
 }
