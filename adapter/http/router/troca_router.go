c22a4abb22b0f56d784e9a1d52be0fd830cfbee2
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-25 13:32:09 -0300

    fix: adicionar scanner de troca

--- adapter/http/router/troca_router.go
+++ adapter/http/router/troca_router.go
@@ -7,15 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupTrocaRoutes(router *gin.Engine, doacaoHandler *handler.TrocaHandler) {
+func SetupTrocaRoutes(router *gin.Engine, pedidoHandler *handler.TrocaHandler) {
 	protectedGroup := router.Group("/api/v1/trocas")
 	protectedGroup.Use(auth.JWTAuthMiddleware())
 	{
-		protectedGroup.POST("", doacaoHandler.RealizarTroca)
-		protectedGroup.GET("/itens-troca", doacaoHandler.ItensDoacao)
-		protectedGroup.POST("/adiciona-item-troca", auth.RoleAuthMiddleware(auth.GetRole()), doacaoHandler.CriaItemTroca)
-		protectedGroup.PUT("/:id", auth.RoleAuthMiddleware(auth.GetRole()), doacaoHandler.AtualizaItemTroca)
-		protectedGroup.DELETE("/:id", auth.RoleAuthMiddleware(auth.GetRole()), doacaoHandler.DeletaItemTroca)
-		protectedGroup.POST("valida-troca", auth.RoleAuthMiddleware(auth.GetRole()), doacaoHandler.ValidaTroca)
+		protectedGroup.POST("", pedidoHandler.RealizarTroca)
+		protectedGroup.POST("/scanner-troca/:id", auth.RoleAuthMiddleware(auth.GetRole()), pedidoHandler.GetTroca)
+		protectedGroup.GET("/itens-troca", pedidoHandler.ItensDoacao)
+		protectedGroup.POST("/adiciona-item-troca", auth.RoleAuthMiddleware(auth.GetRole()), pedidoHandler.CriaItemTroca)
+		protectedGroup.PUT("/:id", auth.RoleAuthMiddleware(auth.GetRole()), pedidoHandler.AtualizaItemTroca)
+		protectedGroup.DELETE("/:id", auth.RoleAuthMiddleware(auth.GetRole()), pedidoHandler.DeletaItemTroca)
+		protectedGroup.POST("valida-troca", auth.RoleAuthMiddleware(auth.GetRole()), pedidoHandler.ValidaTroca)
 	}
 }
