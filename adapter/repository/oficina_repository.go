30d027e6c4c95d3ecf0e3e09ec680fa64b8b39cf
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-25 13:33:51 -0300

    fix: ajuste no fluxo de inscrever participante na oficina

--- adapter/repository/oficina_repository.go
+++ adapter/repository/oficina_repository.go
@@ -3,8 +3,10 @@ package repository
 import (
 	"boi-marronzinho-api/domain"
 	"boi-marronzinho-api/dto"
+	"boi-marronzinho-api/minio"
 	"bytes"
 	"errors"
+	"fmt"   
 	"time"
 
 	"github.com/google/uuid"
@@ -14,11 +16,10 @@ import (
 
 type OficinaRepository interface {
 	Repository[domain.Oficinas]
-	InscreverParticipante(oficinaID uuid.UUID, usuario *domain.Usuario, pagamentoEmBoicoins bool) error
-	BuscarUsuarioPorCodigo(codigoTicket string) (*domain.Usuario, error) // Nova função para buscar usuário por código
+	InscreverParticipante(oficinaID uuid.UUID, usuario *domain.Usuario, pagamentoEmBoicoins bool) (*domain.ParticipanteOficina, error)
+	BuscarUsuarioPorCodigo(codigoTicket string) (*domain.Usuario, error)
 	GetTicketsByUsuarioID(usuarioID uuid.UUID) ([]dto.VoucherResponseDTO, error)
 	ValidaVoucher(codigo *string) (*domain.TicketOficina, error)
-
 }
 
 type oficinaRepository struct {
@@ -33,71 +34,78 @@ func NewOficinaRepository(db *gorm.DB) OficinaRepository {
 	}
 }
 
-func (r *oficinaRepository) InscreverParticipante(oficinaID uuid.UUID, usuario *domain.Usuario, pagamentoEmBoicoins bool) error {
-	var oficina domain.Oficinas
+func (r *oficinaRepository) InscreverParticipante(oficinaID uuid.UUID, usuario *domain.Usuario, pagamentoEmBoicoins bool) (*domain.ParticipanteOficina, error) {
+    var oficina domain.Oficinas
 
-	if err := r.db.First(&oficina, "id = ?", oficinaID).Error; err != nil {
-		return err
-	}
+    if err := r.db.First(&oficina, "id = ?", oficinaID).Error; err != nil {
+        return nil, err
+    }
 
-	if pagamentoEmBoicoins {
-		transacao := domain.BoicoinsTransacoes{
-			ID:            uuid.New(),
-			UsuarioID:     usuario.ID,
-			Quantidade:    -float64(oficina.PrecoBoicoins),
-			TipoTransacao: "inscricao_oficina",
-			Descricao:     "Inscrição na oficina " + oficina.Nome,
-			DataTransacao: time.Now(),
-			OficinaID:     &oficina.ID,
-		}
+    var participanteExistente domain.ParticipanteOficina
+    if err := r.db.Where("usuario_id = ? AND oficina_id = ?", usuario.ID, oficinaID).First(&participanteExistente).Error; err == nil {
+        return nil, errors.New("usuário já está inscrito nesta oficina")
+    }
 
-		if err := r.db.Create(&transacao).Error; err != nil {
-			return err
-		}
-	}
+    if pagamentoEmBoicoins {
+        transacao := domain.BoicoinsTransacoes{
+            ID:            uuid.New(),
+            UsuarioID:     usuario.ID,
+            Quantidade:    -float64(oficina.PrecoBoicoins),
+            TipoTransacao: "inscricao_oficina",
+            Descricao:     "Inscrição na oficina " + oficina.Nome,
+            DataTransacao: time.Now(),
+            OficinaID:     &oficina.ID,
+        }
 
-	oficina.ParticipantesAtual++
-	if err := r.db.Save(&oficina).Error; err != nil {
-		return err
-	}
+        if err := r.db.Create(&transacao).Error; err != nil {
+            return nil, err
+        }
+    }
 
-	codigoTicket := uuid.New().String()
+    oficina.ParticipantesAtual++
+    if err := r.db.Save(&oficina).Error; err != nil {
+        return nil, err
+    }
 
-	var png []byte
-	png, err := qrcode.Encode(codigoTicket, qrcode.Medium, 256)
-	if err != nil {
-		return err
-	}
+    codigoTicket := uuid.New().String()
+    png, err := qrcode.Encode(codigoTicket, qrcode.Medium, 256)
+    if err != nil {
+        return nil, err
+    }
+    qrCodeFileName := fmt.Sprintf("%s.png", codigoTicket)
 
-	var qrCodeBuffer bytes.Buffer
-	qrCodeBuffer.Write(png)
+    qrCodeURL, err := minio.UploadFile(bytes.NewReader(png), qrCodeFileName, "qrcodes")
+    if err != nil {
+        return nil, err
+    }
 
-	inscricao := domain.TicketOficina{
-		ID:        uuid.New(),
-		UsuarioID: usuario.ID,
-		OficinaID: oficina.ID,
-		Codigo:    codigoTicket,
-		QRCode:    qrCodeBuffer.Bytes(),
-		CreatedAt: time.Now(),
-	}
+    inscricao := domain.TicketOficina{
+        ID:        uuid.New(),
+        UsuarioID: usuario.ID,
+        OficinaID: oficina.ID,
+        Codigo:    codigoTicket,
+        QRCode:    qrCodeURL,
+        CreatedAt: time.Now(),
+    }
 
-	if err := r.db.Create(&inscricao).Error; err != nil {
-		return err
-	}
+    if err := r.db.Create(&inscricao).Error; err != nil {
+        return nil, err
+    }
 
-	po := &domain.ParticipanteOficina{
-		ID:            uuid.New(),
-		UsuarioID:     usuario.ID,
-		OficinaID:     oficina.ID,
-		DataInscricao: time.Now(),
-	}
+    po := &domain.ParticipanteOficina{
+        ID:            uuid.New(),
+        UsuarioID:     usuario.ID,
+        OficinaID:     oficina.ID,
+        DataInscricao: time.Now(),
+    }
 
-	if err := r.db.Create(&po).Error; err != nil {
-		return err
-	}
+    if err := r.db.Create(&po).Error; err != nil {
+        return nil, err
+    }
 
-	return nil
+    return po, nil
 }
+
 
 func (r *oficinaRepository) BuscarUsuarioPorCodigo(codigoTicket string) (*domain.Usuario, error) {
 	var ticket domain.TicketOficina
@@ -118,7 +126,7 @@ func (r *oficinaRepository) GetTicketsByUsuarioID(usuarioID uuid.UUID) ([]dto.Vo
 	var results []dto.VoucherResponseDTO
 
 	err := r.db.Table("ticket_oficina").
-		Select("ticket_oficina.id, ticket_oficina.usuario_id, ticket_oficina.oficina_id, ticket_oficina.qr_code, ticket_oficina.created_at, oficinas.nome AS nome_oficina, oficinas.descricao AS descricao").
+		Select("ticket_oficina.id, ticket_oficina.usuario_id, ticket_oficina.oficina_id, ticket_oficina.qr_code, ticket_oficina.created_at, ticket_oficina.validado, oficinas.nome AS nome_oficina, oficinas.descricao AS descricao").
 		Joins("JOIN oficinas ON ticket_oficina.oficina_id = oficinas.id").
 		Where("ticket_oficina.usuario_id = ?", usuarioID).
 		Scan(&results).Error
