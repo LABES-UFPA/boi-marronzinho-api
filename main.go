a6ea22502f0a1d89a022832ac332ae380c10b641
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-25 13:25:19 -0300

    feat: adicionando provider do minio mais cron na main

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"boi-marronzinho-api/adapter/http"
+	"boi-marronzinho-api/core/usecase"
+	"boi-marronzinho-api/minio"
 	"boi-marronzinho-api/module/boicoin"
+	"boi-marronzinho-api/module/evento"
+	"boi-marronzinho-api/module/loja"
 	"boi-marronzinho-api/module/oficina"
 	"boi-marronzinho-api/module/troca"
 	"boi-marronzinho-api/module/user"
@@ -13,14 +17,22 @@ import (
 
 func main() {
 	app := fx.New(
-		fx.Provide(postgres.InitDB),
-		http.RouterModule(),
-		user.UserModule,
-		troca.TrocaModule,
-		boicoin.BoicoinModule,
-		oficina.OficnaModule,
-		fx.Invoke(http.RegisterRoutes),
+		fx.Provide(postgres.InitDB),      
+		fx.Provide(minio.InitMinio),      
+		http.RouterModule(),              
+		user.UserModule,                  
+		troca.TrocaModule,                
+		boicoin.BoicoinModule,            
+		oficina.OficnaModule,             
+		loja.LojaModule,                  
+		evento.EventoModule,              
+		fx.Invoke(http.RegisterRoutes),   
+		fx.Invoke(iniciarCronJobs),
 	)
 
 	app.Run()
+}
+
+func iniciarCronJobs(tuc *usecase.TrocaUseCase) {
+	tuc.IniciarCronJobExpiracaoTroca()
 }
