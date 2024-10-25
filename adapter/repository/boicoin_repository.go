11df0c93b3d2a770cb89af1649261e16217dea93
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-25 13:32:34 -0300

    feat: adicioando metodo que soma saldo boicoins

--- adapter/repository/boicoin_repository.go
+++ adapter/repository/boicoin_repository.go
@@ -1,22 +1,39 @@
 package repository
 
 import (
-    "boi-marronzinho-api/domain"
-    "gorm.io/gorm"
+	"boi-marronzinho-api/domain"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
 )
 
 type BoicoinRepository interface {
-    Repository[domain.BoicoinsTransacoes]
+	Repository[domain.BoicoinsTransacoes]
+	GetSaldoBoicoins(usuarioID uuid.UUID) (float32, error)
 }
 
 type boicoinRepository struct {
-    Repository[domain.BoicoinsTransacoes]
-    db *gorm.DB
+	Repository[domain.BoicoinsTransacoes]
+	db *gorm.DB
 }
 
 func NewBoicoinRepository(db *gorm.DB) BoicoinRepository {
-    return &boicoinRepository{
-        Repository: NewRepository[domain.BoicoinsTransacoes](db),
-        db:         db,
-    }
+	return &boicoinRepository{
+		Repository: NewRepository[domain.BoicoinsTransacoes](db),
+		db:         db,
+	}
+}
+
+func (r *boicoinRepository) GetSaldoBoicoins(usuarioID uuid.UUID) (float32, error) {
+	var saldoTotal float32
+	err := r.db.Table("boicoins_transacoes").
+		Select("SUM(quantidade)").
+		Where("usuario_id = ?", usuarioID).
+		Scan(&saldoTotal).Error
+
+	if err != nil {
+		return 0, err
+	}
+
+	return saldoTotal, nil
 }
