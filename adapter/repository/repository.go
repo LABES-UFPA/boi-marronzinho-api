a6a2d8bbcf8b8316307e86f478c59731b5912075
Logan Cardoso <logan.cc@outlook.com>
2024-10-25 15:25:22 -0300

    fix: adicionando ogs no repository de default

--- adapter/repository/repository.go
+++ adapter/repository/repository.go
@@ -1,68 +1,91 @@
 package repository
 
 import (
-    "github.com/google/uuid"
-    "gorm.io/gorm"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
 type Repository[T any] interface {
-    Create(entity *T) (*T, error)
-    Update(entity *T) (*T, error)
-    Delete(id uuid.UUID) error
-    GetByID(id uuid.UUID) (*T, error)
-    GetAll() ([]*T, error)
-    WithTransaction(fn func(txRepo Repository[T]) error) error
+	Create(entity *T) (*T, error)
+	Update(entity *T) (*T, error)
+	Delete(id uuid.UUID) error
+	GetByID(id uuid.UUID) (*T, error)
+	GetAll() ([]*T, error)
+	WithTransaction(fn func(txRepo Repository[T]) error) error
 }
 
 type repository[T any] struct {
-    db *gorm.DB
+	db *gorm.DB
 }
 
 func NewRepository[T any](db *gorm.DB) Repository[T] {
-    return &repository[T]{db: db}
+	return &repository[T]{db: db}
 }
 
 func (r *repository[T]) Create(entity *T) (*T, error) {
-    if err := r.db.Create(entity).Error; err != nil {
-        return nil, err
-    }
-    return entity, nil
+	logrus.Infof("Criando nova entidade: %+v", entity)
+	if err := r.db.Create(entity).Error; err != nil {
+		logrus.Errorf("Erro ao criar entidade: %v", err)
+		return nil, err
+	}
+	logrus.Infof("Entidade criada com sucesso: %+v", entity)
+	return entity, nil
 }
 
 func (r *repository[T]) Update(entity *T) (*T, error) {
-    if err := r.db.Save(entity).Error; err != nil {
-        return nil, err
-    }
-    return entity, nil
+	logrus.Infof("Atualizando entidade: %+v", entity)
+	if err := r.db.Save(entity).Error; err != nil {
+		logrus.Errorf("Erro ao atualizar entidade: %v", err)
+		return nil, err
+	}
+	logrus.Infof("Entidade atualizada com sucesso: %+v", entity)
+	return entity, nil
 }
 
 func (r *repository[T]) Delete(id uuid.UUID) error {
-    var entity T
-    if err := r.db.Delete(&entity, id).Error; err != nil {
-        return err
-    }
-    return nil
+	logrus.Infof("Deletando entidade com ID: %s", id)
+	var entity T
+	if err := r.db.Delete(&entity, id).Error; err != nil {
+		logrus.Errorf("Erro ao deletar entidade com ID %s: %v", id, err)
+		return err
+	}
+	logrus.Infof("Entidade deletada com sucesso com ID: %s", id)
+	return nil
 }
 
 func (r *repository[T]) GetByID(id uuid.UUID) (*T, error) {
-    var entity T
-    if err := r.db.First(&entity, id).Error; err != nil {
-        return nil, err
-    }
-    return &entity, nil
+	logrus.Infof("Buscando entidade com ID: %s", id)
+	var entity T
+	if err := r.db.First(&entity, id).Error; err != nil {
+		logrus.Errorf("Erro ao buscar entidade com ID %s: %v", id, err)
+		return nil, err
+	}
+	logrus.Infof("Entidade encontrada com ID: %s", id)
+	return &entity, nil
 }
 
 func (r *repository[T]) GetAll() ([]*T, error) {
-    var entities []*T
-    if err := r.db.Find(&entities).Error; err != nil {
-        return nil, err
-    }
-    return entities, nil
+	logrus.Infof("Buscando todas as entidades")
+	var entities []*T
+	if err := r.db.Find(&entities).Error; err != nil {
+		logrus.Errorf("Erro ao buscar todas as entidades: %v", err)
+		return nil, err
+	}
+	logrus.Infof("Encontradas %d entidades", len(entities))
+	return entities, nil
 }
 
 func (r *repository[T]) WithTransaction(fn func(txRepo Repository[T]) error) error {
-    return r.db.Transaction(func(tx *gorm.DB) error {
-        txRepo := &repository[T]{db: tx}
-        return fn(txRepo)
-    })
+	logrus.Infof("Iniciando transação")
+	return r.db.Transaction(func(tx *gorm.DB) error {
+		txRepo := &repository[T]{db: tx}
+		err := fn(txRepo)
+		if err != nil {
+			logrus.Errorf("Erro durante a transação: %v", err)
+			return err
+		}
+		logrus.Infof("Transação concluída com sucesso")
+		return nil
+	})
 }
