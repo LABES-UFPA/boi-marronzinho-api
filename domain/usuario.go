2aad86c08b5339bb5978e9f90a60e1b50067c3fa
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-25 13:39:51 -0300

    fix: alterando nomeclaturas

--- domain/usuario.go
+++ domain/usuario.go
@@ -12,18 +12,18 @@ type Usuario struct {
 	ID                   uuid.UUID      `json:"id" gorm:"primaryKey"`
 	FirstName            string         `json:"firstName" validate:"required"`
 	LastName             string         `json:"lastName" validate:"required"`
-	Email                string         `json:"email" validate:"required"`
-	TipoUsuario          string         `json:"tipoUsuario"`
-	IdiomaPreferido      string         `json:"idiomaPreferido"`
-	Password             string         `json:"password" validate:"required" gorm:"-"`
+	Email                string         `json:"email" validate:"required,email"`                                    // Validação para formato de email
+	TipoUsuario          string         `json:"tipoUsuario" validate:"required,oneof=Cliente Administrador Gringo"` // Validação para tipos de usuário permitidos
+	IdiomaPreferido      string         `json:"idiomaPreferido" validate:"required"`
+	Password             string         `json:"password" validate:"required,min=6" gorm:"-"`
 	PasswordHash         string         `json:"-" validate:"required,min=1,max=255"`
-	SaldoBoicoins        float32        `json:"-"`
-	LastLogin            *time.Time     `json:"-"`
-	PasswordResetToken   *string        `json:"-"`
-	PasswordResetExpires *time.Time     `json:"-"`
-	CreatedAt            time.Time      `json:"-"`
-	UpdatedAt            time.Time      `json:"-"`
-	DeletedAt            gorm.DeletedAt `json:"-"`
+	SaldoBoicoins        float32        `json:"-" gorm:"default:0.00"`
+	LastLogin            *time.Time     `json:"lastLogin,omitempty"`
+	PasswordResetToken   *string        `json:"passwordResetToken,omitempty"`
+	PasswordResetExpires *time.Time     `json:"passwordResetExpires,omitempty"`
+	CreatedAt            time.Time      `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt            time.Time      `json:"updatedAt" gorm:"autoUpdateTime"`
+	DeletedAt            gorm.DeletedAt `json:"deletedAt,omitempty" gorm:"index"`
 }
 
 func (u *Usuario) TableName() string {
