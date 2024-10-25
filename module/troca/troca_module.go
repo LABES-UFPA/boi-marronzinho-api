6b303fbe863b9027a457b5f1d417fd3d15acf50e
Logan Cardoso <logan.cardoso@dgtallab.com.br>
2024-10-25 13:29:30 -0300

    fix: ajuste no repository de troca

--- module/troca/troca_module.go
+++ module/troca/troca_module.go
@@ -1,39 +1,39 @@
 package troca
 
 import (
-	"boi-marronzinho-api/adapter/http/handler"
-	"boi-marronzinho-api/adapter/repository"
-	"boi-marronzinho-api/core/usecase"
-	"boi-marronzinho-api/domain"
+    "boi-marronzinho-api/adapter/http/handler"
+    "boi-marronzinho-api/adapter/repository"
+    "boi-marronzinho-api/core/usecase"
+    "boi-marronzinho-api/domain"
 
-	"go.uber.org/fx"
-	"gorm.io/gorm"
+    "go.uber.org/fx"
+    "gorm.io/gorm"
 )
 
 var TrocaModule = fx.Options(
-	fx.Provide(NewTrocaRepository),
-	fx.Provide(NewItemTrocaRepository),
-	fx.Provide(NewTrocaUseCase),
-	fx.Provide(NewTrocaHandler),
+    fx.Provide(NewTrocaRepository),
+    fx.Provide(NewItemTrocaRepository),
+    fx.Provide(NewTrocaUseCase),
+    fx.Provide(NewTrocaHandler),
 )
 
-func NewTrocaRepository(db *gorm.DB) repository.Repository[domain.Troca] {
-	return repository.NewTocaRepository(db)
+func NewTrocaRepository(db *gorm.DB) repository.TrocaRepository {
+    return repository.NewTrocaRepository(db)
 }
 
 func NewItemTrocaRepository(db *gorm.DB) repository.Repository[domain.ItemTroca] {
-	return repository.NewRepository[domain.ItemTroca](db)
+    return repository.NewRepository[domain.ItemTroca](db)
 }
 
 func NewTrocaUseCase(
-	trocaRepo repository.Repository[domain.Troca],
-	ItemTrocaRepo repository.Repository[domain.ItemTroca],
-	usuarioRepo repository.UserRepository,
-	boicoinRepo repository.BoicoinRepository,
+    trocaRepo repository.TrocaRepository,
+    ItemTrocaRepo repository.Repository[domain.ItemTroca],
+    usuarioRepo repository.UserRepository,
+    boicoinRepo repository.BoicoinRepository,
 ) *usecase.TrocaUseCase {
-	return usecase.NewTrocaUseCase(trocaRepo, ItemTrocaRepo, usuarioRepo, boicoinRepo)
+    return usecase.NewTrocaUseCase(trocaRepo, ItemTrocaRepo, usuarioRepo, boicoinRepo)
 }
 
 func NewTrocaHandler(tuc *usecase.TrocaUseCase) *handler.TrocaHandler {
-	return handler.NewTrocaHandler(tuc)
+    return handler.NewTrocaHandler(tuc)
 }
