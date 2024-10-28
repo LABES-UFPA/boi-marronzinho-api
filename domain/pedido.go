9ed9cf3211ce726c33ea598be6109a42874fc825
Logan Cardoso <logan.cc@outlook.com>
2024-10-28 20:44:15 -0300

    fix: atualizando module de de pedidos

--- domain/pedido.go
+++ domain/pedido.go
@@ -8,19 +8,16 @@ import (
 )
 
 type Pedidos struct {
-	ID             uuid.UUID  `json:"id" gorm:"primaryKey"`
-	UsuarioID      uuid.UUID  `json:"usuarioId" validate:"required"`
-	ProdutoID      uuid.UUID  `json:"produtoId" validate:"required"`
-	OficinaID      uuid.UUID  `json:"oficinaId" validate:"required"`
-	StatusPedido   string     `json:"statusPedido" validate:"required"` // Enum deve ser tratado em outro lugar
-	EnderecoID     uuid.UUID  `json:"enderecoId" validate:"required"`
-	PontoMapaID    uuid.UUID  `json:"pontoMapaId" validate:"required"`
-	BoicoinsUsados float64    `json:"boicoinsUsados" gorm:"default:0.00" validate:"gte=0"`
-	PrecoRealUsado float64    `json:"precoRealUsado" validate:"required,gte=0"`
-	Quantidade     int        `json:"quantidade" gorm:"default:1" validate:"gte=1"`
-	Codigo         string     `json:"codigo" validate:"required"`
-	DataPedido     time.Time  `json:"dataPedido" gorm:"autoCreateTime"`
-	DataConclusao  *time.Time `json:"dataConclusao"`
+	ID             uuid.UUID     `json:"id" gorm:"primaryKey"`
+	UsuarioID      uuid.UUID     `json:"usuarioId" validate:"required"`
+	StatusPedido   string        `json:"statusPedido" validate:"required"` // Enum deve ser tratado em outro lugar
+	BoicoinsUsados float64       `json:"boicoinsUsados" gorm:"default:0.00" validate:"gte=0"`
+	PrecoRealUsado float64       `json:"precoRealUsado" validate:"required,gte=0"`
+	Quantidade     int           `json:"quantidade" gorm:"default:1" validate:"gte=1"`
+	Codigo         string        `json:"codigo" validate:"required"`
+	PedidoItens    []PedidoItens `json:"pedidoItens" gorm:"foreignKey:PedidoID"`
+	DataPedido     time.Time     `json:"dataPedido" gorm:"autoCreateTime"`
+	DataConclusao  *time.Time    `json:"dataConclusao"`
 }
 
 func (p *Pedidos) TableName() string {
@@ -30,4 +27,16 @@ func (p *Pedidos) TableName() string {
 func (p *Pedidos) Validate() error {
 	validate := validator.New()
 	return validate.Struct(p)
+}
+
+type PedidoItens struct {
+	ID            uuid.UUID `json:"id" gorm:"primaryKey"`
+	PedidoID      uuid.UUID `json:"pedidoId" gorm:"not null"`                // Referência ao pedido
+	ProdutoID     uuid.UUID `json:"produtoId" gorm:"not null"`               // Referência ao produto
+	Quantidade    int       `json:"quantidade" validate:"required,gte=1"`    // Quantidade de produtos
+	PrecoUnitario float64   `json:"precoUnitario" validate:"required,gte=0"` // Preço unitário do produto
+}
+
+func (pi *PedidoItens) TableName() string {
+	return "boi_marronzinho.pedido_itens"
 }
