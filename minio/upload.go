1e9438ec745b5ce4feac7c9115df522be8e881ac
Logan Cardoso <logan.cc@outlook.com>
2024-10-29 23:44:56 -0300

    feat: adicionaod novos metodows de adição e deletar imagens do bucket

--- minio/upload.go
+++ minio/upload.go
@@ -9,11 +9,6 @@ import (
     "github.com/minio/minio-go/v7"
 )
 
-// const (
-//     oficinasBucket = "oficinas" 
-//     qrcodesBucket  = "qrcodes" 
-// )
-
 func UploadFile(file io.Reader, fileName string, bucketName string) (string, error) {
     if MinioClient == nil {
         return "", fmt.Errorf("MinioClient não está inicializado")
@@ -37,13 +32,48 @@ func UploadFile(file io.Reader, fileName string, bucketName string) (string, err
 
     _, err = MinioClient.PutObject(context.Background(), bucketName, fileName, file, -1, minio.PutObjectOptions{ContentType: contentType})
     if err != nil {
-        return "", err
+        return "", fmt.Errorf("erro ao fazer upload do arquivo: %v", err)
     }
 
     return fmt.Sprintf("/%s/%s", bucketName, fileName), nil
 }
 
+func DeleteFile(fileName string, bucketName string) error {
+    if MinioClient == nil {
+        return fmt.Errorf("MinioClient não está inicializado")
+    }
+
+    err := MinioClient.RemoveObject(context.Background(), bucketName, fileName, minio.RemoveObjectOptions{})
+    if err != nil {
+        return fmt.Errorf("erro ao deletar arquivo: %v", err)
+    }
+
+    return nil
+}
+
+func UpdateFile(file io.Reader, fileName string, bucketName string) (string, error) {
+    if MinioClient == nil {
+        return "", fmt.Errorf("MinioClient não está inicializado")
+    }
+
+    _ = DeleteFile(fileName, bucketName)
+
+    contentType, err := detectContentType(file)
+    if err != nil {
+        return "", fmt.Errorf("erro ao detectar tipo de conteúdo: %v", err)
+    }
+
+    _, err = MinioClient.PutObject(context.Background(), bucketName, fileName, file, -1, minio.PutObjectOptions{ContentType: contentType})
+    if err != nil {
+        return "", fmt.Errorf("erro ao atualizar arquivo: %v", err)
+    }
+
+    return fmt.Sprintf("/%s/%s", bucketName, fileName), nil
+}
+
+
 func detectContentType(file io.Reader) (string, error) {
+    // Lê os primeiros 512 bytes para detectar o tipo de conteúdo
     buffer := make([]byte, 512)
     _, err := file.Read(buffer)
     if err != nil {
